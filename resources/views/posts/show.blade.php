c200a9a49db7792be943cc22412523085f829826
nbeerten <tutlermax@hotmail.com>
2022-11-27 13:20:24 +0100

    View counter

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,6 +26,14 @@
 
 @push('scripts')
     <script defer src='{{ mix('/js/prism.js', 'dist') }}'></script>
+    <script>
+        var r = new XMLHttpRequest();
+        r.addEventListener('load', function() {
+            document.querySelector('#stats').innerText = JSON.parse(this.responseText).count
+        })
+        r.open('GET', 'https://nilsbeerten.goatcounter.com/counter/' + location.pathname + '.json')
+        r.send()
+    </script>
 @endpush
 
 <x-layout.app>
@@ -34,11 +42,12 @@
             <hgroup>
                 <h1 class="heading">{{ $page->title }}</h1>
                 <div class="information">
-                    <span>
-                        <x-heroicon-o-newspaper/>
-                        <time datetime="{{ $page->date->isoFormat("YYYY-MM-DD") }}" x-data="{ time: new Date({{ $page->date->getTimestampMs() }}).toLocaleString('en', { month: 'long', day: 'numeric', year: 'numeric' }) }"><span x-text="time"></span></time>
-                    </span>
                     <p>Posted by {{ $page->author->name }}</p>
+                    <p>
+                        <x-heroicon-o-newspaper/>
+                        <time style="position: relative" data-popup="Published at" aria-label="Published at" datetime="{{ $page->date->isoFormat("YYYY-MM-DD") }}" x-data="{ time: new Date({{ $page->date->getTimestampMs() }}).toLocaleString('en', { month: 'long', day: 'numeric', year: 'numeric' }) }"><span x-text="time"></span></time>
+                        <span style="position: relative" data-popup="Page views" aria-label="Page views"><x-heroicon-s-eye/> <span id="stats">1</span></span>
+                    </p>
                 </div>
             </hgroup>
             <hr>
@@ -54,6 +63,8 @@
                     <figure>
                         <img src="{{ $page->featured_image }}">
                     </figure>
+                    <div class="views">
+                    </div>
                 </aside>
             </section>
         </article>
