db0f798882c7b4bf05eb3fdb6be7e45d1d12ce5b
nbeerten <59444993+nbeerten@users.noreply.github.com>
2022-12-16 15:52:02 +0100

    Fix outdated preloads--

--- resources/views/background.blade.php
+++ resources/views/background.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Background</title>
+    <title>Background thing</title>
+    <meta name="robots" content="noindex">
+    
     <style>
         body {
             background-color: #111;
