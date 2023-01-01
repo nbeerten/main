59a6aa8d2ae81c57df0084df181d700d3af5547a
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-01-01 22:31:47 +0100

    Fix

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::get('/posts', [PostController::class, 'index'])
 Route::get('/post/{slug}', [PostController::class, 'show'])
     ->name('post.slug');
 Route::redirect('/post', '/posts', 301);
-Route::get('/tags/{tag}', [PostController::class, 'tags'])
-    ->name('post.slug');
+Route::get('/tags/{slug}', [PostController::class, 'tags'])
+    ->name('tags.slug');
 
 Route::get('/contact', [PageController::class, 'contact'])
     ->name('contact');
